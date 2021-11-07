eda6ac9509ebcd783e64b8219ce9aef21fd84deb
TheBotlyNoob <thebotlynoob@gmail.com>
2021-11-07 14:44:24 -0500

    update

--- examples/simple.rs
+++ examples/simple.rs
@@ -1,33 +1,33 @@
-extern crate graceful;
+extern crate better_graceful;
 
 use std::sync::atomic::{AtomicBool, Ordering};
 
+use better_graceful::SignalGuard;
 use std::thread;
 use std::time::Duration;
-use graceful::SignalGuard;
 
 #[macro_use]
 extern crate lazy_static;
 
 lazy_static! {
-    static ref STOP: AtomicBool = AtomicBool::new(false);
+  static ref STOP: AtomicBool = AtomicBool::new(false);
 }
 
 fn main() {
-    let signal_guard = SignalGuard::new();
+  let signal_guard = SignalGuard::new();
 
-    let handle = thread::spawn(|| {
-        println!("Worker thread started. Type Ctrl+C to stop.");
-        while !STOP.load(Ordering::Acquire) {
-            println!("working...");
-            thread::sleep(Duration::from_millis(500));
-        }
-        println!("Bye.");
-    });
+  let handle = thread::spawn(|| {
+    println!("Worker thread started. Type Ctrl+C to stop.");
+    while !STOP.load(Ordering::Acquire) {
+      println!("working...");
+      thread::sleep(Duration::from_millis(500));
+    }
+    println!("Bye.");
+  });
 
-    signal_guard.at_exit(move |sig| {
-        println!("Signal {} received.", sig);
-        STOP.store(true, Ordering::Release);
-        handle.join().unwrap();
-    });
+  signal_guard.at_exit(move |sig| {
+    println!("Signal {} received.", sig);
+    STOP.store(true, Ordering::Release);
+    handle.join().unwrap();
+  });
 }
